be40c910bcbf6c4eb60e077c611f49a2f03cb292
Daniel Haigh <daniel.haigh@oasis.com>
2013-05-10 21:46:45 +1000

    .

--- web/handlers/home.js
+++ web/handlers/home.js
@@ -1,12 +1,59 @@
 
+/*
+
+[{},{},{}]
+
+photoitem
+
+item={
+	href="",
+	title="",
+	src="",
+	description="",
+	date="",
+	user="",
+	likes=""
+}
+
+*/
+
 var memberData 	= require('../persist/member');
 
 global.handlers.addTemplate("page/home",function(req,res,cb) {
 
+	var photo_items=[
+		{
+			href="",
+			title="",
+			src="",
+			description="",
+			date="",
+			user="",
+			likes=""
+		},
+		{
+			href="",
+			title="",
+			src="",
+			description="",
+			date="",
+			user="",
+			likes=""
+		},
+		{
+			href="",
+			title="",
+			src="",
+			description="",
+			date="",
+			user="",
+			likes=""
+		}
+	];
 
 	memberData.get(1,function(err,data) {
 		if (err) {console.log(err);return cb(err);}
-		return cb(null,{page:'home',title:'Home',user:data.member_id});
+		return cb(null,{page:'home',title:'Home',user:data.member_id,photo_items:photo_items});
 	});
  
 
