8057830118e39d481b41e972a0a0aedb55aab5bf
Daniel Haigh <daniel.haigh@oasis.com>
2013-05-21 21:18:09 +1000

    .

---
+++ web/handlers/person.js
@@ -0,0 +1,13 @@
+var Account = require('mongoose').model('Account');
+
+
+global.handlers.addPage("page/person",function(req,res,cb) {
+
+	Account.findById(req.params.id,function(err,acc) {
+		// If can't find then need to search by username
+		// If still can't find then throw a 404 error!
+		return cb(null,{page:'person',title:'Person',account:acc});
+	});
+
+	
+}); 
