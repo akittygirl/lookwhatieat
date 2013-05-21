dcd8d349e45d42aae402326c62f214d914e7e69c
Daniel Haigh <daniel.haigh@oasis.com>
2013-05-21 13:32:34 +1000

    .

--- web/handlers/account.js
+++ web/handlers/account.js
@@ -1,4 +1,8 @@
 
+var Account = require('mongoose').model('Account');
+
+
+
 global.handlers.addPage("page/account",function(req,res,cb) {
 	return cb(null,{page:'account',title:'Account'});
 }); 
@@ -14,8 +18,22 @@ global.handlers.addCreateCall("account",function(req,res,formData,cb) {
 		return cb(null,{ok:false,message:"The passwords don't match."});
 	}
 
-	
+	delete formData.confirm_password
 
+	var account=new Account({
+		email:formData.email,
+		password:formData.password,
+		active:true
+	});
+
+	account.save(function(err,_account) {
+		if (err) return cb(null,{ok:false,saveError:err});
+
+		// Need to create a session and log them in! (INCOMPLETE)
+
+
+		return cb(null,{ok:true,redirect:"/"+_account.id});
+	});
 
 	
 }); 
